f56975ec7469e2e88abcfeb55c7c0f010d921312
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-06-04 14:21:04 -0500

    added consts to lib from Hazard

--- src/root.zig
+++ src/root.zig
@@ -4,9 +4,12 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub const Hazard = @import("Hazard.zig");
+const Hazard = @import("Hazard.zig");
 pub const HazardPointer = @import("Hazard.zig").HazardPointer;
-// HazardPointer
 pub const HazardGuard = @import("Hazard.zig").HazardGuard;
+
+pub const MAX_HAZARDS_PER_THREAD = Hazard.MAX_HAZARDS_PER_THREAD;
+pub const MAX_RETIRED_POINTERS = Hazard.MAX_RETIRED_POINTERS;
+
 
 
